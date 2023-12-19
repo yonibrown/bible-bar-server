d6811da3a8c823e0f98a92086a2b13afddb70497
Jonathan Braun <yonibrown@gmail.com>
2023-12-20 00:11:10 +0200

    upload3

--- obj_file.php
+++ obj_file.php
@@ -1,98 +1,152 @@
 <?php
+
 include 'connect_db.php';
+include 'res_func.php';
 
-exit_error($_POST['oper']);
+$type = $_POST['type'];
+$id = json_decode($_POST['id'],true);
+$oper = $_POST['oper'];
+$prop =  json_decode($_POST['prop'],true);
+$reload = array();
 
-$sql = "DELETE FROM research_points
-        WHERE research_id =  ".$_POST['res']."
-          AND category_id =  ".$_POST['cat'];
-$result = mysqli_query($con,$sql);
-if (!$result) {
-    exit_error('Error description1: ' . mysqli_error($con));
-}
+// exit_error($id['res']);
 
-
-$file = file_get_contents($_FILES['pts']['tmp_name']);
-
+$file = file_get_contents($_FILES['file']['tmp_name']);
 $file = str_replace(array('"','('),'',$file);
 // $file = preg_replace('/\s+|\"|\(/','',$file);
 
-$pt_id = 0;
+$reply = array();
 
-$fileArr = explode('תנך/',$file);
-for ($file_i=0;$file_i<count($fileArr);$file_i++){
-    $lineArr = preg_split("/\n|\/|\)|\,/", $fileArr[$file_i]);
-    if (count($lineArr) >= 4){
-        //division
-        $division_heb = array_shift($lineArr);
+switch ($type){
+    case "research":
+        switch ($oper) {
+            // // set research attributes
+            case "upload_parts":
+                res_DICTA_upload($id,$file);
+                break;
+        }
+        break;    
+}
 
-        //book
-        $book_heb = str_replace('ספר ','',array_shift($lineArr));
-        $book = get_book($book_heb);
+// --------------------------------------------------------------------------------------
+// ---- 
+// --------------------------------------------------------------------------------------
+function res_DICTA_upload($id,$file){
+    global $con;
 
-        //chapter
-        $chapter_heb = str_replace('פרק ','',array_shift($lineArr));
-        $chapter = array_search($chapter_heb,$heb_num);
+    $colObj = res_new_collection($id,array("name"=>"מקובץ"));
 
-        //verses
-        while(count($lineArr)>0){
-            $verse_heb = str_replace('פסוק ','',array_shift($lineArr));
-            if ($verse_heb != ''){
-                $verse = array_search($verse_heb,$heb_num);
-                $text = array_shift($lineArr);
-                add_verse();
+    $part_id = 0;
+    $fileArr = explode('תנך/',$file);
+    for ($file_i=0;$file_i<count($fileArr);$file_i++){
+        $lineArr = preg_split("/\n|\/|\)|\,/", $fileArr[$file_i]);
+        if (count($lineArr) >= 4){
+            $bibleRange = array("from"=>0,"to"=>999999999);
+
+            //division
+            $division_heb = array_shift($lineArr);
+
+            //book
+            $book_heb = str_replace('ספר ','',array_shift($lineArr));
+            $bookRange = res_DICTA_get_range($book_heb,2,$bibleRange);
+
+            //chapter
+            $chapter_heb = str_replace('פרק ','',array_shift($lineArr));
+            $chapterRange = res_DICTA_get_range($chapter_heb,1,$bookRange);
+            // $chapter = array_search($chapter_heb,$heb_num);
+
+            //verses
+            while(count($lineArr)>0){
+                $nxt = array_shift($lineArr);
+                if (str_contains($nxt, 'פסוק ')){
+                    $verse_heb = str_replace('פסוק ','',$nxt);
+                    if ($verse_heb != ''){
+                        // $verse = array_search($verse_heb,$heb_num);
+                        $verseRange = res_DICTA_get_range($verse_heb,0,$chapterRange);
+                        // $text = array_shift($lineArr);
+                        // add_verse($book,$chapter,$verse,$text,$part_id);
+
+                        if(is_null($verseRange)){
+                            exit_error($verse_heb);
+                        }
+
+                        res_new_part($id,array(
+                            "collection_id"=>$colObj['id'],
+                            "src_research"=>1,
+                            "src_collection"=>1,
+                            "src_from_position"=>$verseRange['from'],
+                            "src_from_word"=>0,
+                            "src_to_position"=>$verseRange['to'],
+                            "src_to_word"=>999
+                        ));
+                    }
+                }
             }
         }
     }
 }
 
-function get_book($book_heb){
+function res_DICTA_get_range($name,$level,$posRange){
     global $con;
-    $sql = "SELECT book_id
-            FROM bible_index
-            WHERE hebrew_name = '".$book_heb."'";
+    $sql = "SELECT division_id,from_position,to_position
+            FROM a_res_idx_division
+            WHERE research_id = 1
+                AND collection_id = 1
+                AND index_id = 1
+                AND level = ".$level."
+                AND from_position >= ".$posRange['from']."
+                AND to_position <= ".$posRange['to']."
+                AND name_heb = '".$name."'";
     $result = mysqli_query($con,$sql);
     if (!$result) {
         exit_error('Error description2: ' . mysqli_error($con));
     }
-    $row = mysqli_fetch_array($result);
-    return $row['book_id'];
-}
-
-function add_verse(){
-    global $book,$chapter,$verse,$text,$heb_num,$con,$pt_id;
-
-    $reduced_text = trim(preg_replace("/\s+/", " ",plain_text($text)));
-    $text_arr = explode('*',$reduced_text);
-    $to = 1;
-    while (count($text_arr)>=2){
-        $from = $to + substr_count(array_shift($text_arr),' ');
-        $to = $from + substr_count(array_shift($text_arr),' ');
-        $pt_id++;
-
-//        echo $pt_id.'-'.$book.'-'.$chapter.'-'.$verse.'-'.$from.'-'.$to.'----------';
-
-        $sql = "INSERT INTO research_points
-                (research_id, 
-                category_id, 
-                point_id, 
-                vrskey, 
-                from_word, to_word, 
-                description) 
-                VALUES (".$_POST['res'].", 
-                ".$_POST['cat'].",
-                ".$pt_id.",
-                CONCAT(LPAD('".$book."',2,'0'),'_',LPAD('".$chapter."',3,'0'),'_',LPAD('".$verse."',3,'0')),
-                ".$from.",".$to.", 
-                '')";
-        $result = mysqli_query($con,$sql);
-        if (!$result) {
-            if (strtok(mysqli_error($con), " ") != 'Duplicate'){
-                exit_error('Error description3: ' . mysqli_error($con));
-            }
-        }
+    if($row = mysqli_fetch_array($result)){
+        return array(
+            "from"=>$row['from_position'],
+            "to"=>$row['to_position']
+        );
     }
+
+    return null;
 }
 
+// function add_verse($book,$chapter,$verse,$text,$part_id){
+//     global $con,$heb_num;
+
+//     $reduced_text = trim(preg_replace("/\s+/", " ",plain_text($text)));
+//     $text_arr = explode('*',$reduced_text);
+//     $to = 1;
+//     while (count($text_arr)>=2){
+//         $from = $to + substr_count(array_shift($text_arr),' ');
+//         $to = $from + substr_count(array_shift($text_arr),' ');
+//         $part_id++;
+
+// //        echo $part_id.'-'.$book.'-'.$chapter.'-'.$verse.'-'.$from.'-'.$to.'----------';
+
+//         $sql = "INSERT INTO research_points
+//                 (research_id, 
+//                 category_id, 
+//                 point_id, 
+//                 vrskey, 
+//                 from_word, to_word, 
+//                 description) 
+//                 VALUES (".$_POST['res'].", 
+//                 ".$_POST['cat'].",
+//                 ".$part_id.",
+//                 CONCAT(LPAD('".$book."',2,'0'),'_',LPAD('".$chapter."',3,'0'),'_',LPAD('".$verse."',3,'0')),
+//                 ".$from.",".$to.", 
+//                 '')";
+//         $result = mysqli_query($con,$sql);
+//         if (!$result) {
+//             if (strtok(mysqli_error($con), " ") != 'Duplicate'){
+//                 exit_error('Error description3: ' . mysqli_error($con));
+//             }
+//         }
+//     }
+// }
+
+
+echo json_encode($reply);
 include 'disconnect_db.php';
 ?>
